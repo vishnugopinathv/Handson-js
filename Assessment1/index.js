f09175f2c720d1ae1496505850053937a021c390
vishnugopinathv <vishnugopinathv@gmail.com>
2022-04-07 22:49:48 +0530

    first

---
+++ Assessment1/index.js
@@ -0,0 +1,10 @@
+let str="abcadeecfb";
+let newset= new Set();
+for(let i=0;i<str.length;i++){
+    newset.add(str[i]);
+}
+let str2="";
+for(let val of newset.values()){
+    str2+=val;
+}
+console.log(str2);
